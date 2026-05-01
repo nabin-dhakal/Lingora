af42f4cb529373187363c13e9031cb1aa558b50b
Nabin Dhakal <nabindhakal336@gmail.com>
2026-05-01 23:27:26 +0545

    commit progress

--- Backend/langs/urls.py
+++ Backend/langs/urls.py
@@ -1,4 +1,8 @@
 from fastapi import APIRouter
+from .models import Language, Course, Lesson, Exercise
+from core.database import get_db
+from sqlalchemy.orm import Session
+from typing import List
 
 router = APIRouter(
     prefix="/langs",
@@ -6,3 +10,8 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+
+@router.get("/")
+async def getlangs(reqest: Request, db: Session = Depends(get_db)) -> List[Language]:
+    return db.query(Language).all()
+    
